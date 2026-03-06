326dedce14f5c27e71a53419606f74dc83642e80
Lesvaries <mathisdupont27@gmail.com>
2026-03-06 23:28:54 +0100

    upgrade config.php

--- services-web/config/config.php
+++ services-web/config/config.php
@@ -1,6 +1,8 @@
 <?php
 define('BASE_PATH', dirname(__DIR__));
 define("PUBLIC_PATH", __DIR__ . '/public');
-define("CORE_PATH", __DIR__ . '/core');
-define("VIEW_PATH", __DIR__ . '/view');
+define("CORE_PATH", __DIR__ . '/app/core');
+define("VIEW_PATH", __DIR__ . '/app/views');
+define("CONTROLLER_PATH", __DIR__ . '/app/controllers');
+define("MODEL_PATH", __DIR__ . "/app/models/")
 ?>
